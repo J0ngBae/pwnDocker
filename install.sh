0931b0ee7ed18b52d6479698ebf50e00ffbfc227
wjdwhdqo <75736901+B3LLz@users.noreply.github.com>
2023-04-25 21:34:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,21 +14,29 @@ argv_process(){
             pwn16)
                 echo "[+] PWN16 install............."
                 docker build -t pwn16 ./pwn16/
+                echo 'alias pwn16="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn16 pwn16:latest /bin/zsh"' >> .bashrc
+                source .bashrc
                 shift
                 ;;
             pwn18)
                 echo "[+] PWN18 install............."
                 docker build -t pwn18 ./pwn18/
+                echo 'alias pwn18="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn18 pwn18:latest /bin/zsh"' >> .bashrc
+                source .bashrc
                 shift
                 ;;
             pwn20)
                 echo "[+] PWN20 install............."
                 docker build -t pwn20 ./pwn20/
+                echo 'alias pwn20="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn20 pwn20:latest /bin/zsh"' >> .bashrc
+                source .bashrc
                 shift
                 ;;
             pwn22)
                 echo "[+] PWN22 install............."
                 docker build -t pwn22 ./pwn22/
+                echo 'alias pwn22="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn22 pwn22:latest /bin/zsh"' >> .bashrc
+                source .bashrc
                 shift
                 ;;
             *)
@@ -55,6 +63,12 @@ then
     docker build -t pwn20 ./pwn20/
     echo "[+] PWN22 install............."
     docker build -t pwn22 ./pwn22/
+    
+    echo 'alias pwn16="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn16 pwn16:latest /bin/zsh"' >> .bashrc
+    echo 'alias pwn18="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn18 pwn18:latest /bin/zsh"' >> .bashrc
+    echo 'alias pwn20="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn20 pwn20:latest /bin/zsh"' >> .bashrc
+    echo 'alias pwn22="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn22 pwn22:latest /bin/zsh"' >> .bashrc
+    source .bashrc
 elif [[ $1 = "-s" && $# -gt 1 ]]
 then
     argv_process "$@"
