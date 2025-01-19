6b61fc0be394cb5ff70481bd6c08b253f3c7bed9
J0ngBae <bellship99@naver.com>
2025-01-19 23:14:25 +0900

    update & bug fix

--- install.sh
+++ install.sh
@@ -15,64 +15,28 @@ print_usage(){
 }
 
 argv_process(){
-    chk_alias=$(<$CURRENT_SH)
-
     opt=$2
     case $opt in
         pwn16)
             echo "[+] PWN16 install............."
-            docker build -t pwn16 ./pwn16/
-
-            if [[ "$chk_alias" =~ "pwn16=~/pwnDocker/utils/run16.sh" ]];
-            then
-                echo "[+] alias already set!"
-            else  
-                echo 'alias pwn16=~/pwnDocker/utils/run16.sh' >> $CURRENT_SH
-            fi
-            shift 0
+            docker build -t pwn16 -f pwn16/Dockerfile .
             ;;
         pwn18)
             echo "[+] PWN18 install............."
-            docker build -t pwn18 ./pwn18/
-
-            if [[ "$chk_alias" =~ "pwn18=~/pwnDocker/utils/run18.sh" ]];
-            then
-                echo "[+] alias already set!"
-            else  
-                echo 'alias pwn18=~/pwnDocker/utils/run18.sh' >> $CURRENT_SH
-            fi
-            shift 0
+            docker build -t pwn18 -f pwn18/Dockerfile .
             ;;
         pwn20)
             echo "[+] PWN20 install............."
-            docker build -t pwn20 ./pwn20/
-
-            if [[ "$chk_alias" =~ "pwn20=~/pwnDocker/utils/run20.sh" ]];
-            then
-                echo "[+] alias already set!"
-            else  
-                echo 'alias pwn20=~/pwnDocker/utils/run20.sh' >> $CURRENT_SH
-            fi
-            shift 0
+            docker build -t pwn20 -f pwn20/Dockerfile .
             ;;
         pwn22)
             echo "[+] PWN22 install............."
-            docker build -t pwn22 ./pwn22/
-
-            if [[ "$chk_alias" =~ "pwn22=~/pwnDocker/utils/run22.sh" ]];
-            then
-                echo "[+] alias already set!"
-            else  
-                echo 'alias pwn22=~/pwnDocker/utils/run22.sh' >> $CURRENT_SH
-            fi
-            shift 0
+            docker build -t pwn22 -f pwn22/Dockerfile .
             ;;
         *)
             echo "There is no Dockerfile"
-            shift 0
             ;;
     esac
-    source $CURRENT_SH
 }
 
 if [ $# -lt 1 ]
@@ -84,19 +48,13 @@ fi
 if [ $1 = "-a" ]
 then
     echo "[+] PWN16 install............."
-    docker build -t pwn16 ./pwn16/
+    docker build -t pwn16 -f pwn16/Dockerfile .
     echo "[+] PWN18 install............."
-    docker build -t pwn18 ./pwn18/
+    docker build -t pwn18 -f pwn18/Dockerfile .
     echo "[+] PWN20 install............."
-    docker build -t pwn20 ./pwn20/
+    docker build -t pwn20 -f pwn20/Dockerfile .
     echo "[+] PWN22 install............."
-    docker build -t pwn22 ./pwn22/
-    
-    echo 'alias pwn16="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn16 pwn16:latest /bin/zsh"' >> ~/\.[a-zA-Z0-9]*shrc
-    echo 'alias pwn18="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn18 pwn18:latest /bin/zsh"' >> ~/\.[a-zA-Z0-9]*shrc
-    echo 'alias pwn20="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn20 pwn20:latest /bin/zsh"' >> ~/\.[a-zA-Z0-9]*shrc
-    echo 'alias pwn22="docker run --rm --privileged --cap-add=SYS_PTRACE --security-opt seccomp:unconfined -it -v $PWD:/root/work --name pwn22 pwn22:latest /bin/zsh"' >> ~/\.[a-zA-Z0-9]*shrc
-    source .bashrc
+    docker build -t pwn22 -f pwn22/Dockerfile .
 elif [[ $1 = "-s" && $# -gt 1 ]]
 then
     argv_process "$@"
